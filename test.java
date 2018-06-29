c438a68cdd4ad97fa570805294ba9f70b60eb244
Sebastiaan Hekner <sebastiaan.hekner@secrid.com>
2018-06-29 16:48:57 +0200

    asdaf
    adfasdf

--- test.java
+++ test.java
@@ -7,3 +7,4 @@ asdfas
 
 Development: "{pullrequest={dataType=pullrequest, state=MERGED, stateCount=1}, json={"cachedValue":{"errors":[],"summary":{"pullrequest":{"overall":{"count":1,"lastUpdated":"2018-06-15T09:58:37.000+0200","stateCount":1,"state":"MERGED","dataType":"pullrequest","open":false},"byInstanceType":{"github":{"count":1,"name":"GitHub"}}}}},"isStale":false}}"
 asdfasdf
+asfasfasfas
