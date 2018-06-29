ebfcf34dff62b26e6beac42f1196d88267c89253
Seboeb-test <40473431+Seboeb-test@users.noreply.github.com>
2018-06-29 16:58:02 +0200

    Revert "Test feature 2 is af"

--- test.java
+++ test.java
@@ -3,8 +3,3 @@
 var awesome = 1;
 jaja
 Weer nieuwe line die ik ga PR' en
-asdfas
-
-Development: "{pullrequest={dataType=pullrequest, state=MERGED, stateCount=1}, json={"cachedValue":{"errors":[],"summary":{"pullrequest":{"overall":{"count":1,"lastUpdated":"2018-06-15T09:58:37.000+0200","stateCount":1,"state":"MERGED","dataType":"pullrequest","open":false},"byInstanceType":{"github":{"count":1,"name":"GitHub"}}}}},"isStale":false}}"
-asdfasdf
-asfasfasfas
